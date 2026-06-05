af474f3951591475bd8c5bfe17fc6509f36f3bce
Huy <22004332@st.vlute.edu.vn>
2026-06-05 13:29:21 +0700

    Add files via upload

---
+++ footernguoidung.php
@@ -0,0 +1,125 @@
+<!-- =====  FOOTER START  ===== -->
+<div class="footer pt_60">
+      <div class="container">
+        <div class="row">
+          <div class="footer-top pb_60 mb_30">
+            <div class="col-xs-12 col-sm-6">
+              <div class="footer-logo"> <a href="index.php"> <img src="ass/images/ad1.jpg" alt="OYEENok"> </a> </div>
+              <div class="footer-desc">Lorem ipsum doLorem ipsum dolor sit amet, consectetur adipisicagna.</div>
+              <div class="footer-desc">Lorem ipsum doLorem ipsum dolor sit amet, consectetur adipisicagna.</div>
+             
+            </div>
+            <!-- =====  testimonial  ===== -->
+            <div class="col-xs-12 col-sm-6">
+              <div class="Testimonial">
+                <div class="client owl-carousel">
+                  <div class="item client-detail">
+                    <div class="client-avatar"> <img alt="" src="ass/images/user1.jpg"> </div>
+                    <div class="client-title"><strong>joseph Lui</strong></div>
+                    <div class="client-designation mb_10"> - php Developer</div>
+                    <p><i class="fa fa-quote-left" aria-hidden="true"></i>Lorem ipsum dolor sit amet, volumus oporteat his at sea in Rem ipsum dolor sit amet, sea in odio ..</p>
+                  </div>
+                  <div class="item client-detail">
+                    <div class="client-avatar"> <img alt="" src="ass/images/user2.jpg"> </div>
+                    <div class="client-title"><strong>joseph Lui</strong></div>
+                    <div class="client-designation mb_10"> - php Developer</div>
+                    <p><i class="fa fa-quote-left" aria-hidden="true"></i>Lorem ipsum dolor sit amet, volumus oporteat his at sea in Rem ipsum dolor sit amet, sea in odio ..</p>
+                  </div>
+                  <div class="item client-detail">
+                    <div class="client-avatar"> <img alt="" src="ass/images/user3.jpg"> </div>
+                    <div class="client-title"><strong>joseph Lui</strong></div>
+                    <div class="client-designation mb_10"> - php Developer</div>
+                    <p><i class="fa fa-quote-left" aria-hidden="true"></i>Lorem ipsum dolor sit amet, volumus oporteat his at sea in Rem ipsum dolor sit amet, sea in odio ..</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- =====  testimonial end ===== -->
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 footer-block">
+            <h6 class="footer-title ptb_20">Thông tin</h6>
+            <ul>
+              <li><a href="#">Về chúng tôi</a></li>
+              <li><a href="#">Thông tin giao hàng</a></li>
+              <li><a href="#">CHính sách bảo mật</a></li>
+              <li><a href="#">Điều kiện & Điều kiện</a></li>
+              <li><a href="contact.html">Liên hệ với chúng tôi</a></li>
+            </ul>
+          </div>
+          <div class="col-md-3 footer-block">
+            <h6 class="footer-title ptb_20">Dịch vụ</h6>
+            <ul>
+              <li><a href="#">Trả lại</a></li>
+              <li><a href="#">Sơ đồ trang web</a></li>
+              <li><a href="#">Danh sách mong muốn</a></li>
+              <li><a href="#">Tài khoản của tôi</a></li>
+              <li><a href="#">Lịch sử đơn hàng</a></li>
+            </ul>
+          </div>
+          <div class="col-md-3 footer-block">
+            <h6 class="footer-title ptb_20">Phần bổ sung</h6>
+            <ul>
+              <li><a href="#">Thương Hiệu</a></li>
+              <li><a href="#">Phiếu quà tặng</a></li>
+              <li><a href="#">Chi nhánh</a></li>
+              <li><a href="#">Khuyến mãi</a></li>
+              <li><a href="#">Bản tin</a></li>
+            </ul>
+          </div>
+          <div class="col-md-3 footer-block">
+            <h6 class="footer-title ptb_20">Địa chỉ liên lạc</h6>
+            <ul>
+              <li>Trường SPKT Vĩnh Long</li>
+              <li>(+037) 5545 981
+                <br> (+012) 666 888</li>
+              <li>leduy113ddss@gmail.com</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer-bottom mt_60 ptb_20">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="social_icon">
+                <ul>
+                  <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                  <li><a href="#"><i class="fa fa-google"></i></a></li>
+                  <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="copyright-part text-center">Lê Phúc Duy - leduy113ddss@gmail.com</div>
+            </div>
+            <div class="col-sm-4">
+              <div class="payment-icon text-right">
+                <ul>
+                  <li><i class="fa fa-cc-paypal "></i></li>
+                  <li><i class="fa fa-cc-visa"></i></li>
+                  <li><i class="fa fa-cc-discover"></i></li>
+                  <li><i class="fa fa-cc-mastercard"></i></li>
+                  <li><i class="fa fa-cc-amex"></i></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- =====  FOOTER END  ===== -->
+  </div>
+  <a id="scrollup"></a>
+  <script src="js/jQuery_v3.1.1.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.magnific-popup.js"></script>
+  <script src="js/jquery.firstVisitPopup.js"></script>
+  <script src="js/custom.js"></script>
+</body>
+
+</html>
